24ce473cb80b1f2f8fd1daf7ec48b75355e10aef
I1HSA <victor.vh1702@gmail.com>
2026-04-13 11:37:09 -0300

    Atividade 2 - valor de compra. Nesse código faço o sistema perguntar ao usuário o valor de sua compra. Se o valor foi maior ou igual a 150, será descontado 2% do valor. Se o valor for abaixo de 150, terá um cashback de 2% do valor atual.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let = numero = Number(prompt("Digite um número e eu direi se é par ou impar:"));
+let = valorCompra = Number(prompt("Digite o valor de sua compra:"));
+let = valorCashback = valorCompra * 0.02;
+let = valorDesconto = valorCompra - valorCashback
+let = valorFinal = valorCompra - valorCashback
 
-if (isNaN(numero)) {
+if (isNaN(valorCompra)) {
     alert('Você não digitou um número!');
 }
 else {
 
-    if (numero % 2 == 0) {
-        alert("Seu número é par.")
+    if (valorCompra>= 150) {
+        alert("Parabéns, você acabou de ganhar um desconto de 2%. O Valor de sua compra é de: R$" + valorFinal + ".");
     }
     else {
-        alert("Seu número é impar.");
+        alert("Este é o valor da sua compra: R$"+ valorCompra + ",00. Há um cahsback de 2% do valor de sua compra atual, resultando em: R$"+ valorCashback.toFixed(2) +".");
     }
 }
